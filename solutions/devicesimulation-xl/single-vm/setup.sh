554a568334a0609fcbeda1c723c92b4d862e08b9
Devis Lucato <dluc@users.noreply.github.com>
2018-08-25 21:00:01 -0700

    test:point new ARM template to temp UI build
    Use a new UI build that allows testing more than 20k devices

--- solutions/devicesimulation-xl/single-vm/setup.sh
+++ solutions/devicesimulation-xl/single-vm/setup.sh
@@ -147,7 +147,9 @@ echo "  aad : {"                                      >> ${WEBUICONFIG_SAFE}
 echo "    tenant: '${PCS_WEBUI_AUTH_AAD_TENANT}',"    >> ${WEBUICONFIG_SAFE}
 echo "    appId: '${PCS_WEBUI_AUTH_AAD_APPID}',"      >> ${WEBUICONFIG_SAFE}
 echo "    instance: '${PCS_WEBUI_AUTH_AAD_INSTANCE}'" >> ${WEBUICONFIG_SAFE}
-echo "  }"                                            >> ${WEBUICONFIG_SAFE}
+echo "  },"                                           >> ${WEBUICONFIG_SAFE}
+echo "  maxDevicesPerSimulation: 10000000,"           >> ${WEBUICONFIG_SAFE}
+echo "  minTelemetryInterval: 10000"                  >> ${WEBUICONFIG_SAFE}
 echo "}"                                              >> ${WEBUICONFIG_SAFE}
 
 echo "var DeploymentConfig = {"                        > ${WEBUICONFIG_UNSAFE}
@@ -158,7 +160,9 @@ echo "  aad : {"                                      >> ${WEBUICONFIG_UNSAFE}
 echo "    tenant: '${PCS_WEBUI_AUTH_AAD_TENANT}',"    >> ${WEBUICONFIG_UNSAFE}
 echo "    appId: '${PCS_WEBUI_AUTH_AAD_APPID}',"      >> ${WEBUICONFIG_UNSAFE}
 echo "    instance: '${PCS_WEBUI_AUTH_AAD_INSTANCE}'" >> ${WEBUICONFIG_UNSAFE}
-echo "  }"                                            >> ${WEBUICONFIG_UNSAFE}
+echo "  },"                                           >> ${WEBUICONFIG_UNSAFE}
+echo "  maxDevicesPerSimulation: 10000000,"           >> ${WEBUICONFIG_UNSAFE}
+echo "  minTelemetryInterval: 10000"                  >> ${WEBUICONFIG_UNSAFE}
 echo "}"                                              >> ${WEBUICONFIG_UNSAFE}
 
 cp -p ${WEBUICONFIG_SAFE} ${WEBUICONFIG}
